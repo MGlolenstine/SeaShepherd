f6c4442abda1859f2975eff31c5784ae30c952d0
MGlolenstine <mglolenstine@gmail.com>
2018-03-17 13:39:58 +0100

    + Added missing IceBerg.h

---
+++ IceBerg.h
@@ -0,0 +1,49 @@
+//
+// Created by notyours on 17/03/18.
+//
+
+#ifndef IGRCA_ICEBERG_H
+#define IGRCA_ICEBERG_H
+
+
+#include "P5c.h"
+
+class IceBerg {
+    PVector pos;
+    PVector dir;
+    PImage img;
+    bool shown = false;
+public:
+    IceBerg(PImage img) {
+        pos.x = random(0, width);
+        pos.y = random(0, height);
+        pos.z = 0;
+        dir.random2D();
+        dir.normalize();
+        dir.mult(2);
+        this->img = img;
+    }
+
+    void show() {
+        image(img, pos.x - 16, pos.y - 16, 32, 32);
+    }
+
+    PVector getLocation() {
+        return pos;
+    }
+
+    void move(){
+        PVector tmp = pos.copy();
+        if(tmp.x > width || tmp.x < 0){
+            dir.x *= -1;
+        }
+        if(tmp.y > height || tmp.y < 0){
+            dir.y *= -1;
+        }
+        tmp.add(dir);
+        pos = tmp.copy();
+    }
+};
+
+
+#endif //IGRCA_ICEBERG_H
